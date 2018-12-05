19b8383e6f35e948db541c103f51d26889c48092
lordsap <jeffrey.lord@sap.com>
2018-12-05 15:57:09 +0000

    Add urls

---
+++ node/controllers/leonardo.controller.js
@@ -0,0 +1,9 @@
+exports.classification = (req, res, next) =>{
+	var url = 'https://sandbox.api.sap.com/ml/imageclassification/classification';
+};
+exports.human = (req, res, next) =>{
+	var url = 'https://sandbox.api.sap.com/ml/humandetection/human-detection';
+};
+exports.face = (req, res, next) =>{
+	var url = 'https://sandbox.api.sap.com/ml/facedetection/face-detection';
+};
